0aff47d96617132b8bcf041bf437f9d61aa2159f
Lemintei <soliteilemintei@gmail.com>
2022-06-24 13:13:45 -0700

    adding story/ data types

---
+++ Basic Data Types Activity/storyTime.js
@@ -0,0 +1,25 @@
+//4-strings
+let name = "Monkey D. Luffy"
+let title = "One Piece"
+let goal = "to become the Pirate King"
+let enemyTitle = "World Goverment "
+
+//3-integers
+let age = 17
+let year = 1500
+let crewNumber = 10
+
+//array
+let Crew = ["Zoro", " Ussop", " Nami", " Sanji", " Chopper", " Robin", " Franky", " Brook", " Yamato"]
+//bolean
+let OnePiece = true;
+
+console.log("Hello my name is " + name + " my dream which i'll make a reality is " + goal)
+console.log("and "+ goal + " I need to find the " + title + ".")
+console.log("I'm " + age + " years old and I am the captin of the Going Merry which hold a total of " + crewNumber + " crew members.")
+console.log("In the current year " + year + " the " + enemyTitle + " controls most of the world with an iron fist.")
+console.log("The " + crewNumber + " crew members on my ship are " + Crew + ". They are the best bunch you could find anywhere.")
+console.log("Wait " + name + " is the One Piece the great tresure left behind by the old Pirate king real.")
+console.log("Yes it is " + OnePiece + " the one piece exists and that I " + name + " will find it and become Pirate King.")
+
+
